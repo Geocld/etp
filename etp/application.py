d5d30b79094ed089fb775d6659971c0a9db63f16
lijiahao <lijiahao@hitour.cc>
2017-08-08 13:30:54 +0800

    update

--- etp/application.py
+++ etp/application.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from etp.serversocket import ServerSocket
 from etp.clientsocket import ClientSocket
 
--- etp/serversocket.py
+++ etp/serversocket.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import socket
 
 class ServerSocket:
-    def __init__(self):
-        pass
+    def __init__(self, mode='localhost', port=8080):
+        self.mode = mode
+        self.port = port
+        if mode == "localhost":
+            self.host = mode
+        elif mode == "public":
+            self.host = socket.gethostname()
+        else:
+            self.host = mode
+
+        # 创建基于TCP的流式socket通信,非阻塞
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setblocking(0)
+        self._socket.bind((self.host, self.port))
 
     def run(self):
-        pass
+        self._socket.listen(5)
+        print 'Waiting for connection...'
